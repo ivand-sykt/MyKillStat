c3bbff2dc8d81e8efd9041f0929ffa9265d60a77
Ivan Demin <tankisu01@mail.ru>
2017-02-24 17:30:11 +0300

    first commit with some TODOs

---
+++ src/NoKill/NoKill.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NoKill;
+
+/* NoKill
+[ ] block creative kills
+[ ] block flying kills
+[ ] add permissions
+[ ] multilanguage
+
+[ ] money for kills?
+*/
+use pocketmine\plugin\PluginBase;
+
+class NoKill extends PluginBase {
+	
+	public function onEnable(){
+		
+	}
+	
+	public function onDisable(){
+		
+	}
+}
